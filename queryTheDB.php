c1bc8425a8bfc4753ac523795f1b954516d66126
David Weinberger <david@weinberger.org>
2016-06-18 09:59:54 -0400

    outfile variable
    made it slightly easier to configure

--- queryTheDB.php
+++ queryTheDB.php
@@ -4,6 +4,10 @@
 // Sample query
 $query = "SELECT * from talks WHERE (tags LIKE '%green%') OR (tags LIKE '%sustainab%') OR (tags LIKE '%ecolog%')  OR (tags LIKE '%environment%') OR (tags LIKE '%conservation%') OR (tags LIKE '%conserving%') OR (tags LIKE '%climate change%') OR (tags LIKE '%resources%')";
 
+
+// name of file of results to write
+$outfile_name="tedTalks.txt";
+
 //Get user and database info from a file you've created like this:
 // {
 	// "mysql":"PATH TO MYSQL",
@@ -28,20 +32,23 @@ $dbh = new mysqli($idjson['mysql'],$idjson['username'],$idjson['password'],$idjs
     if ($result->num_rows > 0) {
     // output data of each row
     $rows = array();
+    print "<p>Writing file...</p>";
     while($row = $result->fetch_assoc()) {
-        echo "title: " . $row["title"]. " - Name: " . $row["author"]. " " . $row["description"]. "<br>";
+      //  echo "title: " . $row["title"]. " - Name: " . $row["author"]. " " . $row["description"]. "<br>";
         $rows[]= $row;
     }
 } else {
     echo "0 results";
 }
 
-print "<h2>Count: " . count($rows) . "</h2>";
+print "<p>Done getting results. Count: " . count($rows) . ". Writing results to $outfile_name</p>";
 //print json_encode($rows);
    
-$f = fopen("greenTED.json", "w");
+$f = fopen($outfile_name, "w");
 fwrite($f, json_encode($rows)); 
 fclose($f);
+
+print"<p>$outfile_name written.</p>";
     
 
 ?>
